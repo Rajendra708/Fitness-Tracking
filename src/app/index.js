eeb9d4c3303f24cdc23772b4c3879df7f6c7c8af
rajendra708 <manisharajendrapatidar@gmail.com>
2025-05-22 13:48:35 +0530

    Setup expo router

---
+++ src/app/index.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,FlatList } from 'react-native';
+// @ts-ignore
+import exercises from '../../assets/data/exercises.json';
+import ExerciseListItem from '../../src/components/ExerciseListItem';
+
+
+export default function App() {
+
+const exercise = exercises[0];
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={exercises} 
+        contentContainerStyle={{gap:5}}
+        keyExtractor={(item , index)=> item.name + index}
+        renderItem={({item, index})=> <ExerciseListItem  item={item} />} 
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'ghostwhite',
+    justifyContent: 'center',
+    padding:10,
+    paddingTop:70
+  }
+});
